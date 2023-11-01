01a0228ec72cdcf7bbb1e5b55dbd846bc22d5128
Phan Anh Son <phananhson24101995@gmail.com>
2023-11-01 11:57:01 +0700

    add 15

---
+++ apis/mCCB.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+// Đọc tệp JSON
+const dataPath = path.join(process.cwd(), 'data', 'MCCB.json');
+// Đọc nội dung tệp JSON
+const jsonData = fs.readFileSync(dataPath, 'utf-8');
+const data = JSON.parse(jsonData)
+
+router.get('/', (req, res) => {
+  const { datetime } = req.query;
+  const filteredData = data.filter(item => {
+    const time = new Date(item.time);
+    const timeQuery = new Date(datetime);
+    return time.getTime() === timeQuery.getTime();
+  });
+
+  return res.status(200).json({
+    success: true,
+    data: filteredData.length > 0 ? filteredData : data
+  });
+});
+
+module.exports = router;
