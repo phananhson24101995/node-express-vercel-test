45f59230906c4171a6eaa4f2ae153b7eb22126c0
Phan Anh Son <phananhson24101995@gmail.com>
2023-11-01 14:11:22 +0700

    add 16

--- apis/inverter.js
+++ apis/inverter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const fs = require('fs');
 const path = require('path');
 // Đọc tệp JSON
-const dataPath = path.join(process.cwd(), 'data', 'AC_Cable.json');
+const dataPath = path.join(process.cwd(), 'data', 'Inverter.json');
 // Đọc nội dung tệp JSON
 const jsonData = fs.readFileSync(dataPath, 'utf-8');
 const data = JSON.parse(jsonData)
